e0f00d86f6a057017c9ff0247852c481ff3903ba
moooooogen <66089268+moooooogen@users.noreply.github.com>
2020-12-29 18:36:31 -0600

    Create bot.py
    Currently gives the League of Legends patch number and the top 30 ranked champions/roles/tier/win rate from u.gg when given the command l!tl in Discord.

---
+++ bot.py
@@ -0,0 +1,88 @@
+import discord
+from discord.ext import commands
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+
+client = commands.Bot(command_prefix="l!")
+
+
+def get_alt_img_text(item):
+    img_html = item.find('img')
+    img_text = img_html['alt']
+    return img_text
+
+
+def get_champion_name(champion):
+    for champion_text in champion.find_all(name="strong"):
+        return champion_text.text
+
+
+@client.event
+async def on_ready():
+    print("League Bot is ready to be used!")
+
+
+@client.command(aliases=['tl', 'TL'])
+async def tierlist(message, *num):
+    # initialize the arrays and other variables that will be used
+    stats = []
+    odd_roles = []
+    even_roles = []
+    all_roles = []
+    odd_champions = []
+    even_champions = []
+    all_champions = []
+    i = 0
+    st = ""
+    base_url = 'https://u.gg/lol/tier-list'
+    # invoke the webdriver
+    browser = webdriver.Chrome(
+        executable_path=r'C:\Users\moooo\Desktop\PycharmWorkspace\webdrivers\chromedriver.exe')
+    browser.get(base_url)
+    html = browser.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
+    soup = BeautifulSoup(html, "html.parser")
+
+    for item in soup.find_all('h1', class_='tier-list'):
+        for item_text in item.find_all(name="span"):
+            st += (item_text.text + "\n")  # retrieves the h1 text and current patch number
+            break
+    for item in soup.find_all('div', class_='rt-tbody'):
+        group = []  # group array variable to keep track of the stat texts that aren't associated with an image
+        for item_text in item.find_all(name="span"):
+            group.append(item_text.text)
+            i += 1
+            if i == 6:
+                i = 0
+                stats.append(group)
+                group = []  # resets the group array variable for the next group
+        for odd_role in soup.find_all('div', class_='rt-td role is-in-odd-row'):
+            odd_roles.append(get_alt_img_text(odd_role))
+        for even_role in soup.find_all('div', class_='rt-td role'):
+            even_roles.append(get_alt_img_text(even_role))
+        for odd_champion in soup.find_all('div', class_='rt-td champion is-in-odd-row'):
+            odd_champions.append(get_champion_name(odd_champion))
+        for even_champion in soup.find_all('div', class_='rt-td champion'):
+            even_champions.append(get_champion_name(even_champion))
+    while True:
+        try:  # merge roles and champions into the same respective array
+            all_roles.append(odd_roles.pop(0))
+            all_roles.append(even_roles.pop(0))
+            all_champions.append(odd_champions.pop(0))
+            all_champions.append(even_champions.pop(0))
+        except IndexError:  # catch the exception when there are no remaining values in the individual arrays
+            break  # exit the while loop
+    count = 0  # variable to keep track of which group is currently being handled
+    for group in stats:
+        st += (group[0] + ". " + all_champions[count] + " " + all_roles[count] + " | " + group[1] + " tier | " + group[
+            2] + " win rate\n")
+        count += 1
+        if count == num:
+            break
+
+    await message.send(st)
+
+    # close the automated browser
+    browser.close()
+
+client.run("INSERT DISCORD TOKEN HERE") # my token is substituted out
